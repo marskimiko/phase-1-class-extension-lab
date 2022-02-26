62182f03a86e42e00d8641ed95eb7cd69cc8267f
marisasaunders <marisaksaunders@gmail.com>
2022-02-26 13:14:50 -0500

    commit tree 16ef222bf45fe5956e8ac2333397c3ac85e90bb8

--- index.js
+++ index.js
@@ -20,26 +20,34 @@ class Polygon {
 class Triangle extends Polygon {
   get isValid() {
     let arrayLength = this.array.length
+    const sideOne = this.array[0];
+    const sideTwo = this.array[1];
+    const sideThree = this.array[2];
 
-    if (arrayLength === 3) {
-      let sideOne = this.array[0];
-      let sideTwo = this.array[1];
-      let sideThree = this.array[2];
-
-      if ((sideOne + sideTwo) > sideThree) {
-        return true;
-      } else if ((sideOne + sideThree) > sideTwo) {
-        return true;
-      } else if ((sideTwo + sideThree) > sideOne) {
-        return true;
-      } else {
-        return false;
-      }
+    if (arrayLength === 3 && (sideOne + sideTwo) > sideThree && (sideOne + sideThree) > sideTwo && (sideTwo + sideThree) > sideOne) {
+      return true;
+    } else {
+      return false;
     }
-    return false;
   }
 }
 
 class Square extends Polygon {
-  // inherits constructor from Pet
+  get isValid() {
+    let sideOne = this.array[0];
+    let sideTwo = this.array[1];
+    let sideThree = this.array[2];
+    let sideFour = this.array[3];
+
+    if (sideOne === sideTwo && sideOne === sideTwo && sideThree === sideFour) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  get area() {
+    let sideOne = this.array[0];
+    return sideOne * sideOne;
+  }
 }
